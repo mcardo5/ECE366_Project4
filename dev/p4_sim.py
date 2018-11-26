8e156355a1b7f62323573a59c1e059be7ae90388
madHatter19 <josephdejoya@gmail.com>
2018-11-25 20:19:34 -0600

    xor is done, multi cycle summary is done, cache behavior for a,b, and d is done. Need to complete pipeline report and fully-associative cache

--- dev/p4_sim.py
+++ dev/p4_sim.py
@@ -7,45 +7,100 @@ verbose = True
 def mips_add(Rs, Rt, Rd, register):
     # Function: $Rd = $Rs + $Rt
     op1 = register[Rs]
-    op1.replace('0x', '')
     op2 = register[Rt]
-    op2.replace('0x', '')
     val = twos_comp(int(op1, 16), 32) + twos_comp(int(op2, 16), 32)
-    print(val)
     register[Rd] = dec_to_bit_hex(val) 
 
 def mips_sub(Rs, Rt, Rd, register):
     # Function: $Rd = $Rs - $Rt
     op1 = register[Rs]
-    op1.replace('0x', '')
     op2 = register[Rt]
-    op2.replace('0x', '')
     val = twos_comp(int(op1, 16), 32) - twos_comp(int(op2, 16),32)
     register[Rd] = dec_to_bit_hex(val)
 
 def mips_addi(Rs, Rt, imm, register):
     # Function: $Rt = $Rt + imm
     op1 = register[Rs]
-    op1.replace('0x', '')
     val = twos_comp(int(op1, 16), 32) + imm
     register[Rt] = dec_to_bit_hex(val) 
 
 def mips_sw(Rs, Rt, imm, d_mem, register):
     # Function: d_mem[$Rs + imm] = $Rt
     val = register[Rt]
-    val.replace('0x', '')
     op1 = register[Rs]
-    op1.replace('0x', '')
     mem_addr = '0x' + format(int(op1, 16) + imm, '04x')
     d_mem[mem_addr] = '0x' + format(int(val, 16), '08x')
 
-def mips_lw(Rs, Rt, imm, d_mem, register):
+def mips_lw(Rs, Rt, imm, d_mem, register, cache):
     # Function: $Rt = d_mem[$Rs + imm]
     op1 = register[Rs]
-    op1.replace('0x', '')
     mem_addr = '0x' + format(int(op1, 16) + imm, '04x')
-    register[Rt] = d_mem[mem_addr]
+    # Cache Statistics
+    mem_addr_bin  = format(int(op1, 16) + imm, '016b')
+    access = cache[len(cache) - 1]
+    miss = cache[len(cache) - 2]
+    hit = cache[len(cache) - 3]
+    dmS4B2 = cache[0]
+    dmS2B4 = cache[1]
+    saS2B8 = cache[3]
+    # a. Direct Mapped, blk sz 4, # blk 2
+    try:
+        register[Rt] = d_mem[mem_addr]
+        valid = '1'
+    except KeyError:
+        d_mem[mem_addr] = '0x00000000'
+        register[Rt] = d_mem[mem_addr]
+        valid = '0'
+    tag = mem_addr_bin[0:11]
+    blk1 = mem_addr_bin[11:12]
+    offset1 = mem_addr_bin[12:len(mem_addr_bin)]
+    blk2 = mem_addr_bin[11:13]
+    offset2 = mem_addr_bin[13:len(mem_addr_bin)]
+    set_idx = mem_addr_bin[11:13]
+    if (access == 0):
+        miss[0] = miss[0] + 1
+        miss[1] = miss[1] + 1
+        saCache = {'': ''}
+        for i in range(0,4):
+            saCache[format(i, '02b')] = ['Empty', 'Empty']
+        saCache[set_idx][0] = tag
+        del saCache['']
+    else:
+        if ((dmS4B2[access-1][3] == blk1) and (valid == '1') and (dmS4B2[access-1][2] == tag)):
+            hit[0] = hit[0] + 1
+        else:
+            miss[0] = miss[0] + 1
+        
+        if ((dmS2B4[access-1][3] == blk2) and (dmS2B4[access-1][1] == valid) and (dmS2B4[access-1][2] == tag)):
+            hit[1] = hit[1] + 1
+        else:
+            miss[1] = miss[1] + 1
 
+        saCache = saS2B8[access-1][3]
+        if ((valid == '1') and ((saCache[set_idx][0] == tag) or (saCache[set_idx][1] == tag))):
+            hit[3] = hit[3] + 1
+        else:
+            #print('here {}'.format(access+1))
+            if (saCache[set_idx][0] == 'Empty'):
+                saCache[set_idx][0] = tag
+            elif (saCache[set_idx][1] == 'Empty'):
+                saCache[set_idx][1] = tag
+            else:
+                saCache[set_idx][0] = tag
+            miss[3] = miss[3] + 1
+        
+    dmS4B2.append([mem_addr, valid, tag, blk1, offset1])
+    dmS2B4.append([mem_addr, valid, tag, blk2, offset2])
+    saS2B8.append([mem_addr, valid, set_idx, saCache, offset2])
+
+    access = access + 1
+    # Update record
+    cache[len(cache) - 1] = access
+    cache[len(cache) - 2] = miss
+    cache[len(cache) - 3] = hit
+    cache[0] = dmS4B2
+    cache[1] = dmS2B4
+    cache[3] = saS2B8
 
 def mips_beq(Rs, Rt, imm, pc, register):
     # Function: if $Rs == $Rt then PC = PC + 4 + imm
@@ -53,7 +108,6 @@ def mips_beq(Rs, Rt, imm, pc, register):
     # At this point PC = PC + 4, pre calculated. Thus, add imm to PC
     # if $Rs == $Rt
     if (register[Rs] == register[Rt]):
-        pc.replace('0x', '')
         pc = '0x' + format(int(pc, 16) + (imm << 2), "04x")
     return pc
 
@@ -63,7 +117,6 @@ def mips_bne(Rs, Rt, imm, pc, register):
     # At this point PC = PC + 4, pre calculated. Thus, add imm to PC
     # if $Rs == $Rt
     if (register[Rs] != register[Rt]):
-        pc.replace('0x', '')
         pc = '0x' + format(int(pc, 16) + (imm << 2), "04x")
     return pc
 
@@ -71,20 +124,30 @@ def mips_slt(Rs, Rt, Rd, register):
     # Function: if $Rs < $Rt, then $Rd = 1
     #                         else $Rd = 0
     op1 = register[Rs]
-    op1.replace('0x','')
     op2 = register[Rt]
-    op2.replace('0x','')
     if (twos_comp(int(op1, 16), 32) < twos_comp(int(op2, 16), 32)):
-        print('\t\t\t\t\tHere: True')
         register[Rd] = '0x00000001'
     else:
-        print('\t\t\t\tHere: False')
         register[Rd] = '0x00000000'
 
-def instr_exe(instr, register, d_mem, pc, multi_cpu, multi_instr):
+def mips_xor(Rs, Rt, Rd, register):
+    # Function: $Rd = $Rs xor $Rt
+    op1 = format(int(register[Rs], 16), '032b')
+    op2 = format(int(register[Rt], 16), '032b')
+    result = []
+    for i in range(0,len(op1)):
+        if (op1[i] == op2[i]):
+            result.append('0')
+        else:
+            result.append('1')
+
+    register[Rd] = '0x' + format(int(''.join(result), 2), '08x')
+
+def instr_exe(instr, register, d_mem, pc, multi, cache):
     # Update pc
-    pc.replace('0x', '')
     pc = '0x' + format(int(pc, 16) + 4, "04x")
+    multi_cpu = multi[0]
+    multi_instr = multi[1]
 
     if (instr[0:6] == '000000'):
         Rs = int(instr[6:11], 2)
@@ -115,7 +178,7 @@ def instr_exe(instr, register, d_mem, pc, multi_cpu, multi_instr):
             # Instruction xor
             multi_cpu[1] = multi_cpu[1] + 1
             multi_instr[2] = multi_instr[2] + 1
-            #mips_xor(Rs, Rt, Rd, register)
+            mips_xor(Rs, Rt, Rd, register)
             if (verbose):
                 print('xor ${}, ${}, ${}'.format(Rd, Rs, Rt))
     else:
@@ -154,10 +217,13 @@ def instr_exe(instr, register, d_mem, pc, multi_cpu, multi_instr):
             # Instruction lw
             multi_cpu[2] = multi_cpu[2] + 1
             multi_instr[7] = multi_instr[7] + 1
-            mips_lw(Rs, Rt, imm, d_mem, register)
+            mips_lw(Rs, Rt, imm, d_mem, register, cache)
             if (verbose):
                 print('lw ${}, 0x{}(${})'.format(Rt, format(imm, '04x'), Rs))
 
+    # Update statistics
+    multi[0] = multi_cpu
+    multi[1] = multi_instr
     return pc
 
 def twos_comp(val, bits):
@@ -195,12 +261,16 @@ def disp_i_mem(i_mem):
     print('Addr' + '\t' + 'Content')
     for i in range(0, len(i_mem)):
         print(mem_addr + '\t' + i_mem[mem_addr])
-        mem_addr.replace('0x', '')
         mem_addr = '0x' + format(int(mem_addr, 16) + 4, "04x")
 
 def disp_d_mem(d_mem):
     print("Data Memory")
     print('Addr' + '\t' + 'Content')
+    try:
+        del d_mem['']
+    except KeyError:
+        d_mem = d_mem
+    
     key_ordered = []
     for key in d_mem:
         key_ordered.append(key)
@@ -216,7 +286,6 @@ def hex_to_bin(hex_dict, bin_dict):
     for i in range(0, len(hex_dict)):
         temp_instr = hex_dict[mem_addr]
         bin_dict[mem_addr] = format(int(temp_instr, 16), "032b")
-        mem_addr.replace('0x', '')
         mem_addr = '0x' + format(int(mem_addr, 16) + 4, "04x")
 
 def main():
@@ -226,8 +295,10 @@ def main():
         print("Choose Instruction Memory to be used in the simulation")
         print("e.g. i_mem_A1")
         #filename = input("Instruction Memory Filename: ")
-        filename = "i_mem_A1.txt"
+        #filename = "i_mem_A1.txt"
         #filename = "i_mem_A2.txt"
+        #filename = "i_mem_B1.txt"
+        filename = "i_mem_B2.txt"
 
         # Check if the file exist
         try:
@@ -240,13 +311,12 @@ def main():
             
             # Remove newline character.
             for i in range(0, len(temp_instr)):
-                temp_instr[i] = temp_instr[i][0:len(temp_instr[i]) - 1]
+                temp_instr[i] = temp_instr[i].replace('\n', '')
             # Convert to dictionary.
             mem_addr = '0x0000'
             instr_hex = {mem_addr: ''}
             for i in range(0, len(temp_instr)):
                 instr_hex[mem_addr] = temp_instr[i]
-                mem_addr.replace('0x', '')
                 mem_addr = '0x' + format(int(mem_addr, 16) + 4, "04x")
                 
 
@@ -286,7 +356,7 @@ def main():
     # Test Add
     
     pc = '0x0000'
-    d_mem = {'0x2000': '0x00000000', '0x2020': '0x00000000'}
+    d_mem = {'': ''}
     instr_count = 0
     # Statistics for MIPS multicycle implementation
     # Record breakdown of 3 / 4 / 5 cycles instruction
@@ -305,9 +375,20 @@ def main():
     # multi_instr[7] = number of lw
     # multi_instr[8] = number of sw
     multi_instr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    multi = [multi_cpu, multi_instr]
+    # Record Cache Behavior
+    hit = [0, 0, 0, 0]
+    miss = [0, 0, 0, 0]
+    access = 0
+    # For direct mapped, block size: 4 words, number of blocks: 2
+    dmS4B2 = []
+    dmS2B4 = []
+    faS2B4 = []
+    saS2B8 = []
+    cache = [dmS4B2, dmS2B4, faS2B4, saS2B8, hit, miss, access]
     while (True):
         old_pc = pc
-        pc = instr_exe(instr_bin[pc], register, d_mem, pc, multi_cpu, multi_instr)
+        pc = instr_exe(instr_bin[pc], register, d_mem, pc, multi, cache)
         instr_count = instr_count + 1
         #disp_reg(register)
         #disp_d_mem(d_mem)
@@ -348,10 +429,50 @@ def main():
     print('\t\tInstruction\t# Cycles\t% Cycle')
     print('\t\tlw\t\t{}\t\t{:.2f}%'.format(multi_instr[7]*5, ((multi_instr[7]*5) / multi_total) * 100))
     print('Pipelined MIPS CPU')
-    
+    print('Cache Access Behavior')
+    hit = cache[len(cache) - 3]
+    miss = cache[len(cache) - 2]
+    print('\ta. Direct Mapped Cache, block size: 4 words, number of blocks: 2')
+    print('\tCache log')
+    for i in range(0, len(cache[0])):
+        print('\t\tlw access: {}'.format(i+1))
+        print('\t\t\tMem Addr: ' + cache[0][i][0])
+        print('\t\t\tValid bit: ' + cache[0][i][1])
+        print('\t\t\tTag: ' + cache[0][i][2])
+        print('\t\t\tBlk Index: {}'.format(int(cache[0][i][3], 2)))
+        print('\t\t\tOffset: {:.0f}'.format(int(cache[0][i][4], 2) / 4))
+    print('\t\tNumber of Hits: {}'.format(hit[0]))
+    print('\t\tNumber of Misses: {}'.format(miss[0]))
+    print('\t\tHit Rate: {:.2f}%'.format((hit[0] / (hit[0] + miss[0]) * 100)))
+    print('\tb. Direct Mapped Cache, block size: 2 words, number of blocks: 4')
+    print('\tCache log')
+    for i in range(0, len(cache[1])):
+        print('\t\tlw access: {}'.format(i+1))
+        print('\t\t\tMem Addr: ' + cache[1][i][0])
+        print('\t\t\tValid bit: ' + cache[1][i][1])
+        print('\t\t\tTag: ' + cache[1][i][2])
+        print('\t\t\tBlk Index: {}'.format(int(cache[1][i][3], 2)))
+        print('\t\t\tOffset: {:.0f}'.format(int(cache[1][i][4], 2) / 4))
+    print('\t\tNumber of Hits: {}'.format(hit[1]))
+    print('\t\tNumber of Misses: {}'.format(miss[1]))
+    print('\t\tHit Rate: {:.2f}%'.format((hit[1] / (hit[1] + miss[1]) * 100)))
+    print('\tc. Fully-Associated Cache, block size: 2 words, number of blocks: 4')
+    print('\td. 2-Way Set-Associative Cache, block size: 2 words, number of blocks: 8, number of sets: 4')
+    for i in range(0, len(cache[3])):
+        print('\t\tlw access: {}'.format(i+1))
+        print('\t\t\tMem Addr: ' + cache[3][i][0])
+        print('\t\t\tValid bit: ' + cache[3][i][1])
+        set_idx = cache[3][i][2]
+        print('\t\t\tSet Index: {}'.format(int(set_idx, 2)))
+        print('\t\t\tWay A Tag: ' + cache[3][i][3][set_idx][0])
+        print('\t\t\tWay B Tag: ' + cache[3][i][3][set_idx][1])
+        print('\t\t\tOffset: {:.0f}'.format(int(cache[1][i][4], 2) / 4))
+    print('\t\tNumber of Hits: {}'.format(hit[3]))
+    print('\t\tNumber of Misses: {}'.format(miss[3]))
+    print('\t\tHit Rate: {:.2f}%'.format((hit[3] / (hit[3] + miss[3]) * 100)))
         
-    print("Complete")
 
+    print("Complete")
 
 
 if __name__ == '__main__':
